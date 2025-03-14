fb7440c3fbb3d3b43a6261222b0b1b6edb8384f6
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-14 19:59:42 +0100

    ADD: show route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,10 +7,30 @@ function index(req, res) {
         if (err) return res.status(500).json({error: "Database error"});
 
         res.json(results);
-    })
+    });
 };
 
+function show(req, res) {
+    const {id} = req.params;
+    const movieSql = "SELECT * FROM `movies` WHERE `id` = ?";
+    const reviewsSql = "SELECT * FROM `movies` JOIN `reviews` ON `movies`.`id` = `reviews`.`movie_id` WHERE `movies`.`id` = ?"
+
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({error: "Database error"});
+
+        const movie = movieResults[0];
+
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({error: "Database error"});
+    
+            movie.reviews = reviewsResults;
+            res.json(movie);
+        });
+    });
+}
+
 export {
-    index
+    index,
+    show
 }
 
