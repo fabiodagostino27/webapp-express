45ae3020a1bf6a9fb717a117015a393e373b1188
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-18 11:35:56 +0100

    ADD: cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import movieRouter from "./routes/movieRouter.js";
 import imagePath from "./middlewares/imagePath.js";
 
 const app = express();
 const port = 3000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_APP
+}));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(imagePath);
