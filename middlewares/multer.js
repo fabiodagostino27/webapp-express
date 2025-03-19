19e9bf4f6cf2b86e7fe3875634319658d855c5d1
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-19 15:39:00 +0100

    ADD: multer

---
+++ middlewares/multer.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: './public/img/books/',
+    filename: (req, file, cb) => {
+      const uniqueName = `${Date.now()}-${file.originalname}`;
+      cb(null, uniqueName);
+    },
+});
+
+const upload = multer({ storage });
+
+export default upload;
